108dcea05456ee690b046131005a94d92a7e5247
devdigital <devdigital.cloud@gmail.com>
2020-02-06 19:04:39 +0000

    wip: add pulse icon and enabled state

--- packages/pulse/src/Decorator.js
+++ packages/pulse/src/Decorator.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import { addons, makeDecorator } from '@storybook/addons'
-import { STORY_CHANGED } from '@storybook/core-events'
 import { Pulse } from './Pulse'
 
 const Decorator = ({ story }) => {
@@ -13,16 +12,12 @@ const Decorator = ({ story }) => {
       setMode(mode)
     }
 
-    const handleStoryChange = () => {
-      channel.emit('pulse/reset')
-    }
-
     channel.on('pulse/change-mode', handleModeChange)
-    channel.on(STORY_CHANGED, handleStoryChange)
+
+    channel.emit('pulse/change-enabled', true)
 
     return () => {
       channel.removeListener('pulse/change-mode', handleModeChange)
-      channel.removeListener('STORY_CHANGED', handleStoryChange)
     }
   }, [])
 
--- packages/pulse/src/Tool.js
+++ packages/pulse/src/Tool.js
@@ -1,27 +1,73 @@
-import React, { useState, useEffect } from 'react'
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+import { useState, useEffect } from 'react'
 import { Icons, IconButton } from '@storybook/components'
 import { STORY_CHANGED } from '@storybook/core-events'
 
+const PulseIcon = ({ ...props }) => (
+  <svg {...props} viewBox="0 0 1024 1024">
+    <polyline
+      points="96 640 224 640 352 128 480 896 608 448 672 640 800 640"
+      style={{
+        fill: 'none',
+        'stroke-linecap': 'round',
+        'stroke-linejoin': 'round',
+        'stroke-width': '64px',
+      }}
+    />
+    <circle
+      cx="864"
+      cy="640"
+      r="64"
+      style={{
+        fill: 'none',
+        'stroke-linecap': 'round',
+        'stroke-linejoin': 'round',
+        'stroke-width': '64px',
+      }}
+    />
+  </svg>
+)
+
 export const Tool = ({ channel }) => {
   const [mode, setMode] = useState('paused')
+  const [enabled, setEnabled] = useState(false)
 
   const setAndEmitMode = mode => {
+    if (!enabled) {
+      return
+    }
+
     setMode(mode)
     channel.emit('pulse/change-mode', mode)
   }
 
   useEffect(() => {
-    channel.on(STORY_CHANGED, () => setMode('paused'))
+    channel.on(STORY_CHANGED, () => {
+      setMode('paused')
+      setEnabled(false)
+    })
+
+    channel.on('pulse/change-enabled', enabled => {
+      setEnabled(enabled)
+    })
   }, [channel])
 
+  const isPaused = mode === 'paused'
   return (
     <IconButton
       key="pulse-control"
-      title={mode === 'paused' ? 'Pulse - Play' : 'Pulse - Pause'}
-      onClick={() => setAndEmitMode(mode === 'paused' ? 'playing' : 'paused')}
+      title={isPaused ? 'Pulse - Play' : 'Pulse - Pause'}
+      onClick={() => setAndEmitMode(isPaused ? 'playing' : 'paused')}
       mode={mode}
+      active={!isPaused}
     >
-      <Icons icon={mode === 'paused' ? 'arrowright' : 'cross'} />
+      <PulseIcon
+        css={{
+          stroke: enabled ? 'currentColor' : '#eee',
+          cursor: enabled ? 'pointer' : 'not-allowed',
+        }}
+      />
     </IconButton>
   )
 }
